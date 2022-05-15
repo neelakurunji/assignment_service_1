848267427c55d89badd629478deb1f63e7a03f3c
venkiayasdi <venkatesh.dharmapuri@ayasdi.com>
2022-05-15 16:03:12 +0530

    Final changes to the assignment

--- extras/install_sonarqube.sh
+++ extras/install_sonarqube.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Author: Venkatesh Dharmapuri
 
-docker pull sonarqube:9.4-community
+docker pull sonarqube:6.7.5
 docker volume create --name sonarqube_data
 docker volume create --name sonarqube_logs
 docker volume create --name sonarqube_extensions
-docker run -itd --rm -p 9000:9000 -v sonarqube_extensions:/opt/sonarqube/extensions sonarqube:9.4-community
+docker run -itd --rm -p 9000:9000 -v sonarqube_extensions:/opt/sonarqube/extensions sonarqube:6.7.5
 
 echo "SonarQube installation successful"
